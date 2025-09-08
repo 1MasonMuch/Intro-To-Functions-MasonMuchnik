d9d314d19d2e416c7b44cd49feb04c60151cd665
1MasonMuch <masonm126@nycstudents.net>
2025-09-08 17:06:16 -0400

    Update test.py

--- test.py
+++ test.py
@@ -4,8 +4,12 @@ t = Turtle()
 
 t.shape('turtle')
 
-t.forward(200)
-
-def message(input):
-    print(input)
-message("Hello Class")
+def square(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+square(200)
