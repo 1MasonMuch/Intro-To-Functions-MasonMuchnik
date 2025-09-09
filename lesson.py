c62f29488a5c9dc2dcddcb22b712cc230a968f2f
1MasonMuch <masonm126@nycstudents.net>
2025-09-09 09:13:57 -0400

    Added Lesson.py
    Just a notes folder.

---
+++ lesson.py
@@ -0,0 +1,9 @@
+#def is to define a function name and the parenthesis are parameteres or inputs.
+#to run a function name it
+
+#Loop example:
+#for i in range(3):
+    #print(i)
+
+#Loops can be used in order to shorten code and reduce the amount of times-
+#you actually have to write stuff. 
